451a999254b264489d15b8d062d997d22b94b28a
PramodKumarAS <145136823+PramodKumarAS@users.noreply.github.com>
2025-07-15 19:56:55 +0530

    Update axios.js

--- Frontend/API/axios.js
+++ Frontend/API/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://bookmyshow01-nqip.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
