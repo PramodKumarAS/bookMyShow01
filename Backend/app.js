0cfb10192e68df25e0209c0a55cc0ce28d000958
PramodKumarAS <145136823+PramodKumarAS@users.noreply.github.com>
2025-07-15 20:24:56 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: ['https://bookmyshow01-1.onrender.com', 'http://localhost:3000'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 
 // //Apply CORS **before** routes
