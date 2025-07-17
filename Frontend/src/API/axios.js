6ff7a1b8cd2576948e0758e23c243b66acf16a9e
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-17 20:30:34 +0530

    api changes

--- Frontend/src/API/axios.js
+++ Frontend/src/API/axios.js
@@ -1,19 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://bookmyshow01-nlay.onrender.com',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
-
-// This will run before every request
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+    headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${localStorage.getItem("token")}`
+    },
+    baseURL: "https://bookmyshow01-nlay.onrender.com"
 });
