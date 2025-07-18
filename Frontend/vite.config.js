98241f7823025fac1d2b74f37a5e3d40fe5d1e49
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-18 21:47:03 +0530

    vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // for dev mode fallback
   }
-});
+})
