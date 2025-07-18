20351b7c2b70ba3c1225c4e069a2b073541fa4ea
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-18 21:21:53 +0530

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,14 @@
-import './App.css'
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
-import Home from './Pages/Home'
-import Register from './Pages/Register'
-import Login from './Pages/Login'
-import {Provider} from 'react-redux'
-import store from './Redux/store'
-import ProtectedRoute from './Pages/ProtectedRoute'
-import Admin from './Pages/Admin'
-import Partner from './Pages/Partner'
-import SingleMovie from './Pages/SingleMovie'
-import BookShow from './Pages/BookShow'
-import Forget from './Pages/Forget'
-import Reset from './Pages/Reset'
-import Profile from './Pages/Profile'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
 
 function App() {
-
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/login' element={<Login/>}></Route>
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
